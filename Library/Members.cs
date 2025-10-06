92c488ce233a2b564a5f79f4abdb31db35f3cabb
b00988601 <b00988601@M-MXB105-C04>
2025-10-06 16:57:10 +0100

    Updates code with a New Members Class.

---
+++ Library/Members.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class Members
+    {
+        private int memberID;
+        private string name;
+        private string address;
+        private int phone;
+
+        public int MemberID
+        {
+            get { return memberID; }
+            set { memberID = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public int Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public Members(int memberID, string name, string address, int phone)
+        {
+            this.memberID = memberID;
+            this.name = name;
+            this.address = address;
+            this.phone = phone;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Member ID: {memberID}");
+            Console.WriteLine($"Member Name: {name}");
+            Console.WriteLine($"member Address: {address}");
+            Console.WriteLine($"Member Phone Number: {phone}");
+            Console.WriteLine();
+        }
+    }
+
+
+}
--- Library/Program.cs
+++ Library/Program.cs
@@ -6,9 +6,14 @@ class Program
     {
         Book mybook = new Book("C# for beginners", "BillGates", "1234");
         Book yourbook = new Book("C#Methods", "Microsoft", "3456778");
+        Console.WriteLine("Currently available books");
         mybook.DisplayInfo();
         yourbook.DisplayInfo();
 
-
+        Members member = new Members(1, "John S", "1 High Street", 0779077909);
+        Members member1 = new Members(2, "Johny S", "14 Low Street", 0745677909);
+        Console.WriteLine("Current library Members");
+        member.DisplayInfo();
+        member1.DisplayInfo();
     }
 }
